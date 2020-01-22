feec196a2f8d712bdb0f8499d40d51fb3cd0be21
Pavel Urban <58266541+browser091@users.noreply.github.com>
2020-01-22 16:56:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 let data = [
   {
-    firstName:'Ashton',
-    lastName:'Kuther',
-    age: 40
-  },{
-    firstName:'Bradley',
-    lastName:'Pitt',
-    age: 54
-  },{
-    firstName:'Hannah',
-    lastName:'Dakota',
+    firstName: 'Ashton',
+    lastName: 'Kutcher',
+    age:40
+  },
+  {
+    firstName: 'Bradley',
+    lastName: 'Pitt',
+    age:54
+  },
+  {
+    firstName: 'Hannah',
+    lastName: 'Dakota',
     age:24
   }
 ]
 
 let lastName = prompt('Enter your last name:');
 lastName = lastName.toLowerCase()
-switch(x){
-  case '':
-       alert('Please, enter your last name!');
+let numb;
+if (lastName === '') {
+    alert('Please, enter your last name!');
+    numb = 2
 }
+for (var i=0; i < data.length; i++){
+if (data[i].firstName.toLowerCase() === lastName){
+    numb = 1; 
+  break;
+} else {}
+}
+if (numb === 1) {
+    alert('user name: '+ data[i].firstName + ' '+ data[i].lastName + '\ruser age: '+data[i].age)
+} else if (numb === 2) {
+} else alert('No results found for tou request')
