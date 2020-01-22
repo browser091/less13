4c8a6f30ecc565e1e886184f654486d94f6afd88
Pavel Urban <58266541+browser091@users.noreply.github.com>
2020-01-22 16:55:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let data = [
+  {
+    firstName:'Ashton',
+    lastName:'Kuther',
+    age: 40
+  },{
+    firstName:'Bradley',
+    lastName:'Pitt',
+    age: 54
+  },{
+    firstName:'Hannah',
+    lastName:'Dakota',
+    age:24
+  }
+]
+
+let lastName = prompt('Enter your last name:');
+lastName = lastName.toLowerCase()
+switch(x){
+  case '':
+       alert('Please, enter your last name!');
+}
